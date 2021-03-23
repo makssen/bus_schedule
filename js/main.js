bf4fe4c15c59a27aadac1547714c26393bbc3d03
makssen <65829190+makssen@users.noreply.github.com>
2021-03-23 09:04:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ const craeteSchedule = (data) => {
 }
 
 const getData = () => {
-    fetch('https://makssen.github.io/bus_schedule/db/schedule.json')
+    fetch('../../bus_schedule/db/schedule.json')
         .then(response => response.json())
         .then(response => {
             craeteSchedule(response);
